37bbe6fb50f47d9e7fe6fbbd3c8520e88c12ac7b
Artlands <artlands@users.noreply.github.com>
2018-09-30 22:26:49 -0500

    react, redux, firebase 1-13

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+  projects:[
+    {id: '1', title:'title1', content:'content1, content2, content3'},
+    {id: '2', title:'title2', content:'content1, content2, content3'},
+    {id: '3', title:'title3', content:'content1, content2, content3'}
+  ]
+}
+const projectReducer = (state = initState, action) => {
+  return state
+}
+
+export default projectReducer
